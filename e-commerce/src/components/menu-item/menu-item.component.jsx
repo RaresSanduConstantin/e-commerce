cc6ada4f099d7c0fd586214542fdc2a51440df52
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-05-28 22:01:10 +0300

    cont

--- e-commerce/src/components/menu-item/menu-item.component.jsx
+++ e-commerce/src/components/menu-item/menu-item.component.jsx
@@ -11,7 +11,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     />
     <div className="content">
       <div className="title">{title.toUpperCase()}</div>
-      <span className="subtitle">SHOP NOW</span>
+      <span className="subtitle">SHOP NOW!</span>
     </div>
   </div>
 );
