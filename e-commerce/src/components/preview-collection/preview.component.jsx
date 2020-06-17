36ec63d50108c8de7bcc9051ba9d6609aa8684d6
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-06-17 23:03:21 +0300

    categorii

--- e-commerce/src/components/preview-collection/preview.component.jsx
+++ e-commerce/src/components/preview-collection/preview.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
+import { Link } from "react-router-dom";
 
 const PreviewCollection = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1 className="title">
+      <Link to={(location) => `shop/${title.toLowerCase()}`}>
+        {title.toUpperCase()}
+      </Link>
+    </h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
